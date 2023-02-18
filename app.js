1f217a9c3c620cb9e29bb9ca9d9ba3ab6a026e26
tdubsxc <trdubs90@gmail.com>
2023-02-18 12:50:23 -0500

    Added the core functions to generate a random password.

---
+++ app.js
@@ -0,0 +1,16 @@
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function randomSymbols() {
+  const symbols = "!@#$%^&*";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
